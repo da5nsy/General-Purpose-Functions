57155c0360fa173205c3b5aaaa16b126c6691489
da5nsy <dannygarside@outlook.com>
2018-12-17 16:14:08 +0000

    testing chromatic spread, following Jenny Bosten talk

---
+++ chromatic_spread_of_refs.m
@@ -0,0 +1,46 @@
+% Do natural surfaces spread more in BY axis?
+% Following Jenny Bosten talk at AVA Xmas 2018
+
+clear, clc
+
+%Vrhel reflectances
+load sur_vrhel_withLabels.mat %DG PTB mod
+load sur_vrhel.mat %standard PTB
+
+%CIE 1931 2deg
+load T_xyz1931.mat
+
+%%
+
+%interpolate to match interval and range
+sur_vrhel_i = SplineSrf(S_vrhel,sur_vrhel,S_xyz1931);
+
+%calculate XYZ
+XYZ = T_xyz1931*sur_vrhel_i;
+XYZ_nat = XYZ(:,[labels_vrhel.nat]);
+
+%%
+
+plotChromaticity
+hold on
+
+scatter(XYZ(1,:)./sum(XYZ),XYZ(2,:)./sum(XYZ),'k.')
+
+%just natural 
+scatter(XYZ_nat(1,:)./sum(XYZ_nat),XYZ_nat(2,:)./sum(XYZ_nat),'g.')
+
+%would be nice to pick out skin/hair as this is probably overly represented
+%here, but this isn't working currently:
+
+% for i = 1:size(sur_vrhel,2)
+%     t_label = string(labels_vrhel(i).label);
+%     if strcmp(labels_vrhel(i).label{1:4},'skin') || strcmp(labels_vrhel(i).label(1:4),'skin') || strcmp(labels_vrhel(i).label(1:4),'skin') 
+%         disp(label(i).label)
+%     end
+% end
+
+%% Conclusion
+
+% There is a greater spread in this dataset, but not so much for natural
+% objects.
+% I should probably do this in MB space.
