84a1450879b13d128ae84b60e3a6e837713fdf75
da5nsy <dannygarside@outlook.com>
2019-08-25 13:27:53 +0100

    tidy

---
+++ DGdisplaydefaults.m
@@ -0,0 +1,16 @@
+function d = DGdisplaydefaults
+
+
+d.s=25;               % display, size
+d.MFA = 0.2;          % Marker Face Alpha
+d.mktrns = 0.3;       % Marker transparency
+set(groot,'defaultfigureposition',[100 100 500 400])
+set(groot,'defaultLineLineWidth',2)
+set(groot,'defaultAxesFontName', 'Courier')
+set(groot,'defaultAxesFontSize',12)
+set(groot,'defaultFigureRenderer', 'painters') %renders pdfs as vector graphics
+set(groot,'defaultfigurecolor','white')
+cols = hsv(10); rng(2);
+set(groot,'defaultAxesColorOrder',cols(randperm(size(cols,1)),:))
+
+end
