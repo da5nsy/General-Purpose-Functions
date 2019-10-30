b664c5ea5cae466d44c5ef54c0ec2104d8aff87e
da5nsy <dannygarside@outlook.com>
2019-10-30 16:30:09 +0000

    update DrawChromaticity

--- drawChromaticity.m
+++ drawChromaticity.m
@@ -8,8 +8,12 @@ function DrawChromaticity(type)
 % Accepted inputs are:
 % - '1931' provides the CIE 1931 chromaticity space
 % - '1964' provides the CIE 1964 chromaticity space
-% - 'upvp' provides the CIE 1976 uniform chromaticity scale diagram (UCS diagram), aka u'v', aka u-prime,v-prime
-% - 'MB2'  provides MacLeod-Boynton-type diagram for 2degree observer
+% - 'Phys2' provides an xy chromaticity space based on the CIE Phys 2 deg observer
+% - 'Phys10' provides an xy chromaticity space based on the CIE Phys 2 deg observer
+% - 'uv' provides CIE 1960 UCS diagram (obsolete)
+% - 'upvp' provides the CIE 1976 UCS diagram, aka u'v', aka u-prime,v-prime
+% - 'MB2'  provides MacLeod-Boynton-type diagram for 2degree observer,
+%   based on Stockman/Sharpe fundamentals
 % - 'MB10' - as above for 10degree
 
 % Example usage: 
@@ -45,6 +49,16 @@ elseif strcmp(type,'1964') % CIE 1964 chromaticity space
     load T_xyz1964.mat T_xyz1964 S_xyz1964
     T_xyz = SplineCmf(S_xyz1964,T_xyz1964,S_xyz1931); %resampling so that I can use the old sRGBs that I already calculated, and keep the appearance comparable accross diagrams. Currently 1931 and 1964 are of same range and interval, but including this line for futureproofing
     SL = [T_xyz(1,:)./sum(T_xyz);T_xyz(2,:)./sum(T_xyz)];
+elseif strcmp(type,'Phys2') %CIE Phys 2 degree
+    load T_xyzCIEPhys2.mat T_xyzCIEPhys2 S_xyzCIEPhys2
+    T_xyz = SplineCmf(S_xyzCIEPhys2,T_xyzCIEPhys2,S_xyz1931); %resampling so that I can use the old sRGBs that I already calculated, and keep the appearance comparable accross diagrams. Currently 1931 and 1964 are of same range and interval, but including this line for futureproofing
+    SL = [T_xyz(1,:)./sum(T_xyz);T_xyz(2,:)./sum(T_xyz)];
+elseif strcmp(type,'Phys10') %CIE Phys 10 degree
+    load T_xyzCIEPhys10.mat T_xyzCIEPhys10 S_xyzCIEPhys10
+    T_xyz = SplineCmf(S_xyzCIEPhys10,T_xyzCIEPhys10,S_xyz1931); %resampling so that I can use the old sRGBs that I already calculated, and keep the appearance comparable accross diagrams. Currently 1931 and 1964 are of same range and interval, but including this line for futureproofing
+    SL = [T_xyz(1,:)./sum(T_xyz);T_xyz(2,:)./sum(T_xyz)];
+elseif strcmp(type,'uv') % CIE uv (1960)
+    SL = xyTouv([T_xyz1931(1,:)./sum(T_xyz1931);T_xyz1931(2,:)./sum(T_xyz1931)],1);
 elseif strcmp(type,'upvp') % CIE u' v' (u-prime, v-prime)
     SL = xyTouv([T_xyz1931(1,:)./sum(T_xyz1931);T_xyz1931(2,:)./sum(T_xyz1931)]);
 elseif strcmp(type,'MB2') % MacLeod Boynton 2degree
@@ -68,6 +82,7 @@ end
 for i = 1:size(sRGB_SL,2)-1
     plot([SL(1,i),SL(1,i+1)],[SL(2,i),SL(2,i+1)],...
         'Color',sRGBLine(:,i),...
+        'LineWidth',2,...
         'HandleVisibility','off'); % This means that it won't show up on legends
     hold on % hold on goes here so that this function can create a new figure if one does not exist, or can overlay an existing figure
     box off % gets rid of outer edge tick marks
@@ -76,11 +91,20 @@ end
 %% Axis labels
 
 if strcmp(type,'1931') % CIE 1931 chromaticity space
-    xlabel('x')
-    ylabel('y')
+    xlabel('x_{1931}')
+    ylabel('y_{1931}')
 elseif strcmp(type,'1964') % CIE 1964 chromaticity space
-    xlabel('x')
-    ylabel('y')
+    xlabel('x_{1964}')
+    ylabel('y_{1964}')
+elseif strcmp(type,'Phys2') % CIE 1964 chromaticity space
+    xlabel('x_{F}') % `subscript F denoting “cone-fundamental-based”' (CIE 170-2:2015, p.3)
+    ylabel('y_{F}')
+elseif strcmp(type,'Phys10') % CIE 1964 chromaticity space
+    xlabel('x_{F,10}') % `subscript F denoting “cone-fundamental-based”' (CIE 170-2:2015, p.3)
+    ylabel('y_{F,10}')
+elseif strcmp(type,'uv') % CIE u' v' (u-prime, v-prime)
+    xlabel('u_{1960}')
+    ylabel('v_{1960}')
 elseif strcmp(type,'upvp') % CIE u' v' (u-prime, v-prime)
     xlabel('u''')
     ylabel('v''')
@@ -92,7 +116,7 @@ elseif strcmp(type,'MB10') % MacLeod Boynton 10degree
     ylabel('{\its}_{MB,10}')
 end
 
-if or(strcmp(type,'1931'),strcmp(type,'upvp'))
+if any([strcmp(type,'1931'),strcmp(type,'upvp'),strcmp(type,'Phys2'),strcmp(type,'Phys10')]) 
     axis equal
     axis([0 1 0 1])
 elseif or(strcmp(type,'MB2'),strcmp(type,'MB10'))
