aa1261492374c4300f09a730ec991a0f860578d6
da5nsy <dannygarside@outlook.com>
2019-11-20 22:48:55 +0000

    at basic working stage

--- CalcDWandExPur.m
+++ CalcDWandExPur.m
@@ -3,34 +3,41 @@ function [dominantWavelength,exPurity] = CalcDWandExPur(xy,xy_white)
 % Calculate Dominant Wavelength and Excitation Purity
 %
 % As per:
-% Schanda, J., 2015. CIE Chromaticity Diagrams, CIE Purity, CIE Dominant Wavelength. In: R. Luo, ed. Encyclopedia of Color Science and Technology. [online] Berlin, Heidelberg: Springer Berlin Heidelberg.pp.1–6. Available at: <http://link.springer.com/10.1007/978-3-642-27851-8_325-1> [Accessed 19 Nov. 2019].
+% - Schanda, J., 2007. Colorimetry: Understanding the CIE System. John Wiley & Sons. pg.65
+% - Schanda, J., 2015. CIE Chromaticity Diagrams, CIE Purity, CIE Dominant Wavelength. In: R. Luo, ed. Encyclopedia of Color Science and Technology. [online] Berlin, Heidelberg: Springer Berlin Heidelberg.pp.1–6. Available at: <http://link.springer.com/10.1007/978-3-642-27851-8_325-1> [Accessed 19 Nov. 2019].
+% - CIE 15.3:2004, pg. 21
+
+% TO DO
+% - rotate data to suit (?)
+% - Fix for when x_white == x? 
+% - Build in support for 1964
+% - sub-nm precision?
+% - Remove / comment out building struts
+
+%%
 
 %clear, clc, close all
 
-% TO DO
-% - Purity for purples
-% - Add option for where xy == xy_white
-% - rotate data to suit (?)
-% - What happens is x_white == x? Does it mess up the trig with
-% infinity/divide by zero stuff?
-% - Build in support for 1964
-% - sub-nm precision?
-
 %% Load CIE data
 
-% if ~exist('xy','var')
-%     xy = [0.5;0.4];
-%     disp('Bugtesting mode')
-% end
-% 
-% if ~exist('xy_white','var')
-%     xy_white = [0.31271;0.32902];
-%     disp('Using default white point (D65)')
-% end
-    
+if ~exist('xy','var')
+    xy = [0.31271;0.4];
+    disp('Bugtesting mode')
+end
+
+if ~exist('xy_white','var')
+    xy_white = [0.31271;0.32902];
+    disp('Using default white point (D65)')
+end
+
+if xy == xy_white
+    error('xy == xy_white, and thus Dominant Wavelength is undefined and Excitation Purity is 0')
+end    
     
 load T_xyz1931.mat T_xyz1931 S_xyz1931
 SL = [T_xyz1931(1,:)./sum(T_xyz1931);T_xyz1931(2,:)./sum(T_xyz1931)];
+
+SL = [SL,[linspace(SL(1,end),SL(1,1),1000);linspace(SL(2,end),SL(2,1),1000)]];
 
 figure, hold on
 plot(SL(1,:),SL(2,:),'k.','HandleVisibility','off')
@@ -47,11 +54,11 @@ c = xy(2) - (m*xy(1));
 %% Generate line of fixed length
 
 xlen = cosd(atand(m(1)))*0.8849; %0.8849 roughly equals the largest distance between arbitrary points in 1931 xy
-incr = xlen/1000;
+incr2 = xlen/1000;
 if xy_white(1) < xy(1) % make sure the line goes towards the spectral locus
-    x = xy_white(1):incr:xy_white(1) + xlen;
+    x = xy_white(1):incr2:xy_white(1) + xlen;
 else
-    x = xy_white(1):-incr:xy_white(1) - xlen;
+    x = xy_white(1):-incr2:xy_white(1) - xlen;
 end
 y = m(1).*x + c(1);
 
@@ -69,45 +76,47 @@ figure, plot(minDistance)
 figure, plot(indexOfMin)
 
 wl = SToWls(S_xyz1931);
-dominantWavelength = wl(indexOfMin(indexOfMinTop));
-
-disp(dominantWavelength)
-
-%% Purple catch
-
-if minDistanceTop > 0.0254/2
-    % then repeat above but with line in opposite direction
+try
+    dominantWavelength = wl(indexOfMin(indexOfMinTop));
+catch
+    % Purple catch
+    % ------------
+    % If there isn't a corresponding wavelength then that means the line
+    % has gone out of the bottom of the diagram through the line of
+    % purples, in which case we re-run everything but with the line going
+    % in the opposite direction to find the corresponding wavelength.
+    % We then report this but negated, to indicate that it is the
+    % corresponding wavelength and not the 'real' dominant wavelength.
     
-    xlen = cosd(atand(m(1)))*0.8849;
-    incr = xlen/1000;
     if xy_white(1) < xy(1)
-        x = xy_white(1):-incr:xy_white(1) - xlen; %signs switched
+        x2 = xy_white(1):-incr2:xy_white(1) - xlen; %signs switched
     else
-        x = xy_white(1):incr:xy_white(1) + xlen; %signs switched
+        x2 = xy_white(1):incr2:xy_white(1) + xlen; %signs switched
     end
-    y = m(1).*x + c(1);
+    y2 = m(1).*x2 + c(1);
     
     figure(1) %adds this back to the original figure
-    plot(x,y,'k.')    
+    plot(x2,y2,'k.')    
     
-    for k = 1 : size(x,2)
-        distances = sqrt((SL(1,:)-x(1,k)).^2 + (SL(2,:)-y(1,k)).^2);
+    for k = 1 : size(x2,2)
+        distances = sqrt((SL(1,:)-x2(1,k)).^2 + (SL(2,:)-y2(1,k)).^2);
         [minDistance(k), indexOfMin(k)] = min(distances);
     end
-    [minDistanceTop, indexOfMinTop] = min(minDistance);
+    [minDistanceTop, indexOfMinTop_2] = min(minDistance);
     
     figure, plot(minDistance)
     figure, plot(indexOfMin)
     
     wl = SToWls(S_xyz1931);
-    dominantWavelength = -wl(indexOfMin(indexOfMinTop)); % provide negated dominantWavelength to denote complementary
-    
-    disp(dominantWavelength)
-    if minDistanceTop > 0.0254/2
-        % if the distance is still greater than half the distance between
-        % adjacent points on the SL...
-        error('something has gone wrong in purple land')
-    end
+    dominantWavelength = -wl(indexOfMin(indexOfMinTop_2)); % provide negated dominantWavelength to denote complementary
+end
+
+disp(dominantWavelength)
+
+if minDistanceTop > 0.0254/2
+    % if the distance is still greater than half the distance between
+    % adjacent points on the SL...
+    error('Somehow, and I don''t know how, the distance between the closest point to the spectral locus, and the closest point on the spectral locus, is higher than it should be.')
 end
 
 %% Calculate Excitation Purity
--- DichrSatTest.m
+++ DichrSatTest.m
@@ -12,6 +12,7 @@ clc, clear, close all
 load T_xyz1931.mat T_xyz1931 S_xyz1931
 load spd_D65.mat spd_D65 S_D65
 load sur_macbeth.mat sur_macbeth S_macbeth
+%load sur_nickerson.mat sur_nickerson S_nickerson
 
 figure, 
 subplot(3,1,1)
@@ -20,6 +21,7 @@ subplot(3,1,2)
 plot(SToWls(S_D65),spd_D65)
 subplot(3,1,3)
 plot(SToWls(S_macbeth),sur_macbeth)
+%plot(SToWls(S_nickerson),sur_nickerson)
 
 %% Colorimetry
 
@@ -29,6 +31,7 @@ if ~isequal(S_xyz1931,S_D65)
 end
 
 XYZ = T_xyz1931*(sur_macbeth.*spd_D65);
+%XYZ = T_xyz1931*(sur_nickerson.*spd_D65);
 xy = [XYZ(1,:)./sum(XYZ); XYZ(2,:)./sum(XYZ)];
 
 XYZ_d65 = T_xyz1931*spd_D65;
@@ -38,6 +41,20 @@ figure,
 DrawChromaticity
 scatter(xy(1,:),xy(2,:),'k*');
 scatter(xy_d65(1,:),xy_d65(2,:),'r*');
+
+%% Calculate dominant wavelengths and purity
+
+for i=1:size(sur_macbeth,2)
+%for i=1:size(sur_nickerson,2)
+    [dominantWavelength(i),exPurity(i)] = CalcDWandExPur(xy(:,i),xy_d65);
+    close all
+end
+
+%% Plot against x
+
+figure,
+
+scatter(xy(1,:),dominantWavelength)
 
 
 
